8d8e57e9f85cd400a25366c8b2b1fbacacd786da
Aleck <aleck.vincent@gmail.com>
2020-06-28 18:49:08 +0200

    initial commit

---
+++ src/test/java/avm/io/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package avm.io.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
